76e4f365f528f06a565aaf412a8a404003912a54
MatiasMtz <4442@holbertonschool.com>
2022-06-13 14:35:02 +0000

    added prototype

--- sort.h
+++ sort.h
@@ -30,5 +30,6 @@ int lomutoPartition(int *array, size_t size, int start, int end);
 void quickSortRecursion(int *array, size_t size, int start, int end);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif
