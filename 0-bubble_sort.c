e650ff78338293a77e079ec8d4802489296ba5d9
MatiasMtz <4442@holbertonschool.com>
2022-06-13 12:13:37 +0000

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * @bubble_sort - sorts an array of integers using the Bubble sort algorithm
+ * @array: unsorted integers array
+ * @size: array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+    unsigned int count = 0, value = 0; 
+	int aux;
+
+	if (size < 2)
+		return;
+
+	for (count = 0; count < size; count++)
+	{
+		for (value = 0; value < (size - count - 1); value++)
+		{
+			if (array[value] > array[value + 1])
+			{
+				aux = array[value];
+				array[value] = array[value + 1];
+				array[value + 1] = aux;
+				print_array(array, size);
+            }
+        }
+    }
+}
