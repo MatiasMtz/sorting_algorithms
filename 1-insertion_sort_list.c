de3dcbcde1fed61a6ba11677198db726540d5521
MatiasMtz <4442@holbertonschool.com>
2022-06-13 14:11:56 +0000

    task 1 added

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers with the 
+ * Insertion sort algorithm
+ * @list: unsorted integer double linked list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *currentNode = NULL, *aux = NULL;
+
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+    currentNode = *list;
+    while (currentNode != NULL)
+    {
+        aux = currentNode;
+        while (currentNode->prev != NULL && currentNode->n < currentNode->prev->n)
+        {
+            aux->prev->next = aux->next;
+            if (aux->next != NULL)
+                aux->next->prev = aux->prev;
+            aux->next = aux->prev;
+            aux->prev = aux->prev->prev;
+            aux->next->prev = aux;
+            if (aux->prev != NULL)
+                aux->prev->next = aux;
+            else
+                *list = aux;
+            print_list(*list);
+        }
+        currentNode = currentNode->next;
+    }
+}
