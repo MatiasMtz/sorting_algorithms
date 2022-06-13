7a4f38d353fc27dbf80f37a11e37218efb63bd71
MatiasMtz <4442@holbertonschool.com>
2022-06-13 12:45:52 +0000

    task 2 added

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array following the selection sort algorithm
+ * @array: array to sort
+ * @size: size of the array;
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned int count = 0, value = 0, countAux = 0;
+	int aux = 0;
+
+	if (size < 2)
+		return;
+
+	for (count = 0; count < size; count++)
+	{
+		countAux = count;
+		for (value = count + 1; value < size; value++)
+		{
+			if (array[value] < array[countAux])
+				countAux = value;
+		}
+		aux = array[count];
+		array[count] = array[countAux];
+		array[countAux] = aux;
+		if (count != countAux)
+			print_array(array, size);
+	}
+}
